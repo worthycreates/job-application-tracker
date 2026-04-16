b5650ea2b3da2c7d318a09c584e9f81193f6e83f
worthycreates <worthyag@icloud.com>
2026-04-16 09:49:48 +0100

    Working on rendering the jobs to the page.

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const jobs = [];
+
 const companyInput = document.querySelector('[data-js="company-name"]');
 const jobForm = document.querySelector('[data-js="job-form"]');
+
+const displayJobs = document.querySelector('[data-js="display-jobs"]');
+
 
 jobForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -11,6 +16,28 @@ jobForm.addEventListener("submit", (e) => {
   }
 
   console.log(`Adding job for: ${name}`);
+  jobs.push(createJob(name));
+  console.log(jobs);
+
   companyInput.value = "";
   companyInput.focus();
+
+  renderJobs(jobs);
 });
+
+
+const createJob = (name) => {
+  return {name};
+};
+
+const renderJobs = (jobs) => {
+  displayJobs.children = "";
+  
+  jobs.forEach(job => {
+    const li = document.createElement("li");
+    li.textContent = job.name;
+    displayJobs.append(li);
+  });
+
+  console.log(displayJobs);
+};
