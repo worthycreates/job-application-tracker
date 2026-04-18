aaddd5734557209eb2837c21809775aaac761a5a
worthycreates <worthyag@icloud.com>
2026-04-18 17:25:44 +0100

    Adding the Mark as Interview btn.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const jobs = [];
 
 const companyInput = document.querySelector('[data-js="company-name"]');
@@ -27,16 +28,30 @@ jobForm.addEventListener("submit", (e) => {
 
 
 const createJob = (company) => {
-  return {company, status: "Applied"};
+  return {
+    id: crypto.randomUUID(),
+    company, 
+    status: "Applied",
+  };
 };
+
+const createJobElement = (job) => {
+  const btn = document.createElement("button");
+  btn.textContent = "Mark as Interview";
+  btn.setAttribute('class', 'action-btn');
+
+  const li = document.createElement("li");
+  li.textContent = `Company: ${job.company}, Status: ${job.status}`;
+  li.append(btn)
+  return li;
+};
+
 
 const renderJobs = (jobs) => {
   displayJobs.replaceChildren();
   
   jobs.forEach(job => {
-    const li = document.createElement("li");
-    li.textContent = `Company: ${job.company}, Status: ${job.status}`;
-    displayJobs.append(li);
+    displayJobs.append(createJobElement(job));
   });
 
   console.log(displayJobs);
