dca7e7f2a57f20be441dca98ff67b2481e770915
worthycreates <worthyag@icloud.com>
2026-04-23 22:22:50 +0100

    Updating logic.

--- app.js
+++ app.js
@@ -53,21 +53,68 @@ const createJob = (company, role) => {
 };
 
 
+// const createJobElement = (job) => {
+//   const btn = document.createElement("button");
+//   btn.textContent = job.status !== "Offer" ? `Mark as ${jobStates[jobStates.indexOf(job.status) + 1]}` : "";
+//   btn.classList.add('action-btn');
+//   btn.setAttribute('data-js', 'action-btns');
+//   btn.setAttribute('data-id', job.id);
+
+//   btn.addEventListener("click", (e) => {
+//     updateStatus(job);
+//   });
+
+//   const li = document.createElement("li");
+//   li.textContent = `Company: ${job.company}, Role: ${job.role}, Status: ${job.status}`;
+//   li.append(btn)
+//   return li;
+// };
+
 const createJobElement = (job) => {
-  const btn = document.createElement("button");
-  btn.textContent = job.status !== "Offer" ? `Mark as ${jobStates[jobStates.indexOf(job.status) + 1]}` : "";
-  btn.classList.add('action-btn');
-  btn.setAttribute('data-js', 'action-btns');
-  btn.setAttribute('data-id', job.id);
+  const btn = buildElement("button", {
+      text: job.status !== "Offer" ? `Mark as ${jobStates[jobStates.indexOf(job.status) + 1]}` : "",
+      classes: ["action-btn"],
+      attr: {
+        "data-js": "action-btns",
+        "data-id": job.id
+      },
+      event: {
+        type: "click",
+        handler: (e) => {
+          console.log("The event object is here:", e);
+          console.log("You clicked the button for:", job.company);
+          updateStatus(job);
+        }
+      }
+    }
+  );
 
-  btn.addEventListener("click", (e) => {
-    updateStatus(job);
-  });
+  const li = buildElement("li", {
+      text: `Company: ${job.company}, Role: ${job.role}, Status: ${job.status}`,
+      classes: ["job-item", "active"],
+      attr: {"data-id": job.id},
+      child: btn
+    }
+  );
 
-  const li = document.createElement("li");
-  li.textContent = `Company: ${job.company}, Role: ${job.role}, Status: ${job.status}`;
-  li.append(btn)
   return li;
+};
+
+const buildElement = (tag, options = {}) => {
+  const { text, classes, attr, event, child } = options;
+  const el = document.createElement(tag);
+
+  if (text) el.textContent = text;
+  if (classes) el.classList.add(...classes);
+  if (attr) Object.entries(attr).forEach(([k, v]) => el.setAttribute(k, v));
+  if (event) el.addEventListener(event.type, event.handler);
+  if (child) el.append(child);
+
+    // el.addEventListener(event.eventType, (e) => {
+    //   event.action(e);
+    // });
+
+  return el;
 };
 
 
