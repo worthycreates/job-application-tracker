2f39e71ebdef77fe01beda92643b7f369f7ea500
worthycreates <worthyag@icloud.com>
2026-04-25 13:58:09 +0100

    Updating the structure of the app.

--- app.js
+++ app.js
@@ -1,138 +1,127 @@
 
-const jobs = [];
-const jobStates = ["Considering", "Applied", "Interview", "Offer"];
+const app = {
+  jobs: [],
+  jobStates: ["Considering", "Applied", "Interview", "Offer"],
 
-const companyInput = document.querySelector('[data-js="company-name"]');
-const roleInput = document.querySelector('[data-js="job-role"]');
-const jobForm = document.querySelector('[data-js="job-form"]');
-const displayJobs = document.querySelector('[data-js="display-jobs"]');
-const actionBtns = document.querySelectorAll('[data-js="action-btns"]');
+  init() {
+    this.companyInput = document.querySelector('[data-js="company-name"]');
+    this.roleInput = document.querySelector('[data-js="job-role"]');
+    this.jobForm = document.querySelector('[data-js="job-form"]');
+    this.displayJobs = document.querySelector('[data-js="display-jobs"]');
+    this.actionBtns = document.querySelectorAll('[data-js="action-btns"]');
 
+    this.jobForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const company = this.companyInput.value.trim();
+        const role = this.roleInput.value.trim();
+        
+        if (!this.requireInput(company, "company") || !this.requireInput(role, "role"))
+          return;
 
-jobForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const company = companyInput.value.trim();
-  const role = roleInput.value.trim();
-  
-  if (!requireInput(company, "company") || !requireInput(role, "role"))
-    return;
+        this.updateState({
+          company,
+          role
+        });
 
-  console.log(`Adding job for: ${company}: ${role}`);
-  jobs.push(createJob(company, role));
-  console.log(jobs);
+        this.clearForm();
+      });
+  },
 
-  clearInput([companyInput, roleInput]);
- 
-  companyInput.focus();
+  clearForm() {
+    this.clearInput([this.companyInput, this.roleInput]);
+    this.companyInput.focus();
+  },
 
-  renderJobs();
-});
+  requireInput(formInput, inputType){
+    if (!formInput) {
+      alert(`Please enter a ${inputType}!`);
+      return false;
+    }
+    return true;
+  },
 
-const requireInput = (formInput, inputType) => {
-  if (!formInput) {
-    alert(`Please enter a ${inputType}!`);
-    return false;
-  }
-  return true;
-};
+  clearInput(inputs) {
+    inputs.forEach(input => {
+      input.value = "";
+    });
+  },
 
-const clearInput = (inputs) => {
-  inputs.forEach(input => {
-    input.value = "";
-  });
-};
+  createJob(company, role) {
+    return {
+      id: crypto.randomUUID(),
+      company, 
+      role,
+      status: this.jobStates[0],
+    };
+  },
 
-
-const createJob = (company, role) => {
-  return {
-    id: crypto.randomUUID(),
-    company, 
-    role,
-    status: jobStates[0],
-  };
-};
-
-
-// const createJobElement = (job) => {
-//   const btn = document.createElement("button");
-//   btn.textContent = job.status !== "Offer" ? `Mark as ${jobStates[jobStates.indexOf(job.status) + 1]}` : "";
-//   btn.classList.add('action-btn');
-//   btn.setAttribute('data-js', 'action-btns');
-//   btn.setAttribute('data-id', job.id);
-
-//   btn.addEventListener("click", (e) => {
-//     updateStatus(job);
-//   });
-
-//   const li = document.createElement("li");
-//   li.textContent = `Company: ${job.company}, Role: ${job.role}, Status: ${job.status}`;
-//   li.append(btn)
-//   return li;
-// };
-
-const createJobElement = (job) => {
-  const btn = buildElement("button", {
-      text: job.status !== "Offer" ? `Mark as ${jobStates[jobStates.indexOf(job.status) + 1]}` : "",
-      classes: ["action-btn"],
-      attr: {
-        "data-js": "action-btns",
-        "data-id": job.id
-      },
-      event: {
-        type: "click",
-        handler: (e) => {
-          console.log("The event object is here:", e);
-          console.log("You clicked the button for:", job.company);
-          updateStatus(job);
+  createJobElement(job) {
+    const btn = this.buildElement("button", {
+        text: job.status !== "Offer" ? `Mark as ${this.jobStates[this.jobStates.indexOf(job.status) + 1]}` : "",
+        classes: ["action-btn"],
+        attr: {
+          "data-js": "action-btns",
+          "data-id": job.id
+        },
+        event: {
+          type: "click",
+          handler: (e) => {
+            console.log("The event object is here:", e);
+            console.log("You clicked the button for:", job.company);
+            this.updateStatus(job);
+          }
         }
       }
+    ),
+
+    li = this.buildElement("li", {
+        text: `Company: ${job.company}, Role: ${job.role}, Status: ${job.status}`,
+        classes: ["job-item", "active"],
+        attr: {"data-id": job.id},
+        child: btn
+      }
+    );
+
+    return li;
+  },
+
+  buildElement(tag, options = {}) {
+    const { text, classes, attr, event, child } = options;
+    const el = document.createElement(tag);
+
+    if (text) el.textContent = text;
+    if (classes) el.classList.add(...classes);
+    if (attr) Object.entries(attr).forEach(([k, v]) => el.setAttribute(k, v));
+    if (event) el.addEventListener(event.type, event.handler);
+    if (child) el.append(child);
+
+    return el;
+  },
+
+  renderJobs() {
+    this.displayJobs.replaceChildren();
+    
+    this.jobs.forEach(job => {
+      this.displayJobs.append(this.createJobElement(job));
+    });
+
+    console.log(this.displayJobs);
+  },
+
+  updateStatus(job) {
+    if (job.status !== "Offer") {
+      job.status = this.jobStates[this.jobStates.indexOf(job.status) + 1];
     }
-  );
+    
+    console.log(job);
+    this.renderJobs();
+  },
 
-  const li = buildElement("li", {
-      text: `Company: ${job.company}, Role: ${job.role}, Status: ${job.status}`,
-      classes: ["job-item", "active"],
-      attr: {"data-id": job.id},
-      child: btn
-    }
-  );
-
-  return li;
+  updateState(jobDetails) {
+    const { company, role } = jobDetails;
+    this.jobs.push(this.createJob(company, role));
+    this.renderJobs();
+  },
 };
 
-const buildElement = (tag, options = {}) => {
-  const { text, classes, attr, event, child } = options;
-  const el = document.createElement(tag);
-
-  if (text) el.textContent = text;
-  if (classes) el.classList.add(...classes);
-  if (attr) Object.entries(attr).forEach(([k, v]) => el.setAttribute(k, v));
-  if (event) el.addEventListener(event.type, event.handler);
-  if (child) el.append(child);
-
-    // el.addEventListener(event.eventType, (e) => {
-    //   event.action(e);
-    // });
-
-  return el;
-};
-
-
-const renderJobs = () => {
-  displayJobs.replaceChildren();
-  
-  jobs.forEach(job => {
-    displayJobs.append(createJobElement(job));
-  });
-
-  console.log(displayJobs);
-};
-
-const updateStatus = (job) => {
-  if (job.status !== "Offer") {
-    job.status = jobStates[jobStates.indexOf(job.status) + 1];
-  }
-  
-  console.log(job);
-  renderJobs();
-};
+app.init();
